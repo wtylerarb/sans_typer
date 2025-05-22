0835d5ba77806a946d08e954536087cd4eb57e7b
wtylerarb <wtylerarb@gmail.com>
2025-05-22 12:50:15 -0400

    Added Megalovania support and updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-Under construction
+# Sans Typer
+
+Sans Typer is a Firefox extension that plays sound effects associated with Sans from the video game Undertale. 
+
+## Features
+- Sans "voice" on default keystrokes
+- Notes from the opening of "Megalovania" on top-row keys (~ to +)
+## 
+
+This extension is heavily inspired by DageXVIII's *Animalese Typing* [Chrome](https://chromewebstore.google.com/detail/animalese-typing/djbgadolfboockbofalipohdncimebic?hl=en&pli=1) and [Firefox](https://addons.mozilla.org/en-US/firefox/addon/animalese-typing/) extension, which is a similar concept, playing Animal Crossing Villager sound effects upon a keystroke. 
--- content.js
+++ content.js
@@ -1,11 +1,23 @@
 const keySounds = {
     // TODO: make Megalovania notes bound to the number keys
-    "1": "megalov1.mp3"
+    "`": "megalovania0.mp3",
+    "1": "megalovania1.mp3",
+    "2": "megalovania2.mp3",
+    "3": "megalovania3.mp3",
+    "4": "megalovania4.mp3",
+    "5": "megalovania5.mp3",
+    "6": "megalovania6.mp3",
+    "7": "megalovania7.mp3",
+    "8": "megalovania8.mp3",
+    "9": "megalovania9.mp3",
+    "0": "megalovania10.mp3",
+    "-": "megalovania11.mp3",
+    "=": "megalovania12.mp3"
 };
 
 
 document.addEventListener("keydown", (e) => {
-    const sound = "sans_voice.mp3";
+    const sound = keySounds[e.key] || "sans_voice.mp3";
     const audio = new Audio(browser.runtime.getURL(`sounds/${sound}`));
     audio.play();
 });
